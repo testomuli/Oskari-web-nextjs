d98e6ea7f88b04e6b84490843fd6a2741d20d8f8
Samuli Välimäki <samuli.valimaki@sitowise.com>
2023-08-31 14:29:13 +0300

    feat: usedby marquee component

--- components/UsedBy.tsx
+++ components/UsedBy.tsx
@@ -9,14 +9,14 @@ const UsedBy = ({
   title: string
 }) => {
   return (
-    <section className={`${styles.usedBy} container-content`}>
+    <section className={`${styles.usedBy} container--content`}>
       <h2 className={styles.usedBy__title}>{title}</h2>
       <div className={`${styles.usedBy__container} ${styles.marquee}`}>
         <div className={styles.usedBy__list}>
           {data?.length > 0 &&
             data.map((item) => (
               <div className={styles['usedBy__list-item']} key={item.src}>
-                <Image src={item.src} alt={item.alt} width={240} height={126} />
+                <Image src={item.src} alt={item.alt} width={240} height={125} />
               </div>
             ))}
         </div>
