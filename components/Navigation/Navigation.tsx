b50c20b72186d1924b3832236d33f205ad48a3c0
Samuli Välimäki <samuli.valimaki@sitowise.com>
2023-09-01 15:30:45 +0300

    feat(navigation): sticky navigation

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -5,30 +5,27 @@ import NavigationMenu from './NavigationMenu'
 
 export default function Navigation() {
   return (
-    <>
-      <header className='nav'>
-        <div className='nav__wrapper container--content'>
-          <div className='nav__container'>
-            <div className='nav__logo'>
-              <Image
-                src='/assets/images/oskari-logo.svg'
-                alt='Oskari Logo'
-                width={200}
-                height={90}
-                priority={true}
-              />
-            </div>
-            <div className='nav__search'>
-              <Search />
-            </div>
-            <NavigationMenu />
-            <div className='nav__cta'>
-              <Button title='Download' variant='primary' />
-            </div>
+    <header className='nav'>
+      <div className='nav__wrapper container--content'>
+        <div className='nav__container'>
+          <div className='nav__logo'>
+            <Image
+              src='/assets/images/oskari-logo.svg'
+              alt='Oskari Logo'
+              width={200}
+              height={90}
+              priority={true}
+            />
+          </div>
+          <div className='nav__search'>
+            <Search />
+          </div>
+          <NavigationMenu />
+          <div className='nav__cta'>
+            <Button title='Download' variant='primary' />
           </div>
         </div>
-      </header>
-      <div className='nav__spacer'></div>
-    </>
+      </div>
+    </header>
   )
 }
