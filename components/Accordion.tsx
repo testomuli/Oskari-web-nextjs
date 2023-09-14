d116e92bc88a885173f661652a232c511369d92c
Samuli Välimäki <samuli.valimaki@sitowise.com>
2023-09-14 16:10:59 +0300

    feat: accordion group: closes opened accordions

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from '@/styles/accordion.module.scss'
 
 export default function Accordion({
@@ -10,13 +10,41 @@ export default function Accordion({
   content: string | React.ReactNode
 }) {
   const [isOpen, setIsOpen] = useState(false)
+  const [isAccordionGroup, setIsAccordionGroup] = useState(false)
+  const accordionRef = useRef<HTMLDivElement>(null)
 
   const toggleAccordion = () => {
+    if (isAccordionGroup && accordionRef.current?.parentElement) {
+      const children = Array.from(accordionRef.current.parentElement.children)
+
+      if (children.length > 0) {
+        children.forEach((child) => {
+          console.log(child.classList)
+          if (child !== accordionRef.current) {
+            child.classList.remove(styles.open)
+            return
+          }
+          child.classList.add(styles.open)
+        })
+      }
+      return
+    }
     setIsOpen((prev) => !prev)
   }
 
+  useEffect(() => {
+    const hasAccordionGroupParent =
+      accordionRef.current?.parentElement?.className.includes('accordionGroup')
+    if (hasAccordionGroupParent) {
+      setIsAccordionGroup(true)
+    }
+  }, [])
+
   return (
-    <div className={`${styles.accordion} ${isOpen && styles.open}`}>
+    <div
+      className={`${styles.accordion} ${isOpen && styles.open}`}
+      ref={accordionRef}
+    >
       <div
         role='button'
         className={styles.accordion__header}
