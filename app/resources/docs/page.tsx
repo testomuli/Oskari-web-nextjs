d776050d6dd073521dca51f324712fffb3d13cef
Samuli Välimäki <samuli.valimaki@sitowise.com>
2023-09-15 11:43:57 +0300

    feat: accordion menu list

--- app/resources/docs/page.tsx
+++ app/resources/docs/page.tsx
@@ -1,32 +1,78 @@
 import React from 'react'
-import Container from '@/components/Container'
 import Layout from '@/components/Layout'
 import { Metadata } from 'next'
 import Accordion from '@/components/Accordion/Accordion'
 import AccordionGroup from '@/components/Accordion/AccordionGroup'
-import Button from '@/components/Button'
-import HighlightBox from '@/components/HighlightBox'
+import Text from '@/components/Text'
+import Link from 'next/link'
+import styles from '@/styles/accordion.module.scss'
 
 export const metadata: Metadata = {
   title: 'Documentation',
 }
 
 export default function DocsPage() {
+  const renderMenuContent = (items: { title: string; to: string }[]) => (
+    <ul className={styles.accordionMenu}>
+      {items?.map((item) => (
+        <li key={item.to}>
+          <Link href={item.to}>{item.title}</Link>
+        </li>
+      ))}
+    </ul>
+  )
+
   return (
     <Layout heroSmall heroTitle='Documentation'>
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '1fr 4fr',
-        }}
-      >
-        <aside>
-          <h3>Select version</h3>
-          <Accordion
-            title='v1.0.0'
-            content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim lorem hac a ultricies. Id ornare turpis vulputate enim sed magna sit. A id cursus dolor urna. Aliquam diam integer vitae eget.'
-          />
-        </aside>
+      <div className='container'>
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: '1fr 4fr',
+            gap: '5vmin',
+          }}
+        >
+          <aside>
+            <h3>Select version</h3>
+            <select>
+              <option value='1.0'>1.0</option>
+            </select>
+            <AccordionGroup>
+              <Accordion
+                title='Application environment'
+                content={renderMenuContent([
+                  { title: '1.1 Frontend', to: 'frontend' },
+                  { title: '1.1 Backend', to: 'backend' },
+                ])}
+              />
+              <Accordion
+                title='Application environment'
+                content={renderMenuContent([])}
+              />
+            </AccordionGroup>
+          </aside>
+          <article>
+            <h2>Application environment</h2>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Text>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Text>
+          </article>
+        </div>
       </div>
     </Layout>
   )
