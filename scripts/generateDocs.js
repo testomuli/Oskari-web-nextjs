f708f0e9e2ff97405b2a12a583340fe29e7150fb
Aleksi Kääriäinen <allu.kaariainen@gmail.com>
2023-11-30 15:35:43 +0200

    changelog is created in its own folder with each section as its own file

--- scripts/generateDocs.js
+++ scripts/generateDocs.js
@@ -34,41 +34,44 @@ fs.cpSync(pathToDocumentationRepository, pathToVersionRoot, {
 });
 console.log('Initialized docs for version ' + version);
 
-
 // https://github.com/oskariorg/oskari-frontend
 const pathToFrontendRepository = path.join(pathToExternalRepos, 'oskari-frontend/');
 const pathToServerRepository = path.join(pathToExternalRepos, 'oskari-server/');
 const pathToBundlesDocumentation = path.join(pathToVersionRoot, '2 Application functionality/');
 
+/*
+* Generate section 5 of documentation
+*/
 
-// Create a summary file highlighting the changes in new version from these files
+const pathToNewFiles = path.join(pathToVersionRoot, '/5 Changelog')
+// Init folder and heading file
+fs.mkdirSync(pathToNewFiles, {recursive: true})
+fs.writeFileSync(path.join(pathToNewFiles, "5 Changelog.md"), "# 5 Changelog\n")
+
+// Create a summary section highlighting the changes in new version from these files
 const filesToHandle = {
-  "Frontend release notes": path.join(pathToFrontendRepository, 'ReleaseNotes.md'),
-  "Frontend changelog": path.join(pathToFrontendRepository, 'api/CHANGELOG.md'),
-  "Server release notes": path.join(pathToServerRepository, 'ReleaseNotes.md'),
-  "Migration guide": path.join(pathToServerRepository, 'MigrationGuide.md')
+  "5.1 Frontend release notes": path.join(pathToFrontendRepository, 'ReleaseNotes.md'),
+  "5.2 Frontend changelog": path.join(pathToFrontendRepository, 'api/CHANGELOG.md'),
+  "5.3 Server release notes": path.join(pathToServerRepository, 'ReleaseNotes.md'),
+  "5.4 Migration guide": path.join(pathToServerRepository, 'MigrationGuide.md')
 }
 
-
-const pathToNewFile = path.join(pathToBundlesDocumentation, 'CHANGELOG.md')
-if (!fs.existsSync(pathToNewFile)) {
-  // Go through the files one by one
-  for (const [sectionTitle, pathToFile] of Object.entries(filesToHandle)) {
+// Go through the files one by one
+for (const [sectionTitle, frontendPath] of Object.entries(filesToHandle)) {
+  const sectionFile = path.join(pathToNewFiles, `${sectionTitle}.md`)
   // Read the file, take the content corresponding to version
-    fileContent = fs.readFileSync(pathToFile, 'utf-8')
-    const start = fileContent.indexOf(version)
-    if (start < 0) {
-      console.log(`No mention of version ${version} in ${pathToFile}`)
-    } else {
-      // Find end of section, otherwise take the rest of the file
-      const end = fileContent.indexOf('\n## ', start + version.length) || content.length - 1
-      const section = fileContent.slice(start + version.length, end).trim()
-      // Write each section to the log file
-      fs.appendFileSync(pathToNewFile, `\n## ${sectionTitle}\n\n` + section)
-    }
+  fileContent = fs.readFileSync(frontendPath, 'utf-8')
+  const start = fileContent.indexOf(version)
+  if (start < 0) {
+    console.log(`No mention of version ${version} in ${pathToFile}`)
+  } else {
+    // Find end of section, otherwise take the rest of the file
+    const end = fileContent.indexOf('\n## ', start + version.length) || content.length - 1
+    const section = fileContent.slice(start + version.length, end).trim()
+    // Write each section to the log file
+    fs.writeFileSync(`${sectionFile}`, `# ${sectionTitle}\n\n${section}`)
   }
 }
-
 // TODO: read bundle docs from pathToFrontendRepository + api/**/**/bundle.md etc
 //  - generate flattened listing of bundles + their requests + events
 //  - maybe as a secondary effort try to create links from bundles using the requests/events to ones providing the
@@ -82,7 +85,7 @@ const bundleFiles = fs.readdirSync(
   .filter(dirent => dirent.name === "bundle.md")
   //.map(dirent => path.join(dirent.path, dirent.name))
 
-const bundleFileName = path.join(pathToVersionRoot, 'bundles.md')
+const bundleFileName = path.join(pathToBundlesDocumentation, 'bundles.md')
 bundleFiles.forEach(
   dirent => {
     const bundleName = path.basename(dirent.path)
