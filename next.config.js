c0034d14ded1cf9e1c791b06acc5d1b4acff8c76
Pekka Helesuo <pekka.helesuo@siili.com>
2024-03-21 11:20:01 +0200

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -11,5 +12,5 @@ const nextConfig = {
     includePaths: [path.join(__dirname, 'styles')],
   },
 }
-
-module.exports = withContentlayer(nextConfig)
+// we need this for now so long as we have refs to contentlayer anywhere in our app.
+module.exports = withContentlayer(nextConfig); //nextConfig;
