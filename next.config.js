1db3dc76072314cd47cb36401750430be909dbc8
Pekka Helesuo <pekka.helesuo@siili.com>
2024-04-09 09:33:30 +0300

    remove contentlayer

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withContentlayer } = require('next-contentlayer')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -12,5 +9,4 @@ const nextConfig = {
     includePaths: [path.join(__dirname, 'styles')],
   },
 }
-// we need this for now so long as we have refs to contentlayer anywhere in our app.
-module.exports = withContentlayer(nextConfig); //nextConfig;
+module.exports = nextConfig;
