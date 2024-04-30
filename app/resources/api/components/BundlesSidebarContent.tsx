fac141e75b2de81da684f151af8316fc6d350533
Pekka Helesuo <pekka.helesuo@siili.com>
2024-04-30 12:41:04 +0300

    add accordions to bundles apidoc sidebar

--- app/resources/api/components/BundlesSidebarContent.tsx
+++ app/resources/api/components/BundlesSidebarContent.tsx
@@ -1,5 +1,28 @@
-import { Bundle } from '@/types/api';
+import Accordion from '@/components/Accordion/Accordion';
+import AccordionGroup from '@/components/Accordion/AccordionGroup';
+import { Bundle, BundleItem } from '@/types/api';
+import styles from '@/styles/accordion.module.scss'
+import Link from 'next/link';
 import slugify from 'slugify';
+
+const renderAccordionContent = (
+  bundles: Array<BundleItem>, baseHref: string
+) => {
+  return (
+    <ul className={styles.accordionMenu}>
+      {bundles?.map((item, index) => {
+        const slug = slugify(item.name)
+        return <li key={slug + '_' + index}>
+          <Link
+            href={baseHref + slug}
+          >
+            {item.name}
+          </Link>
+        </li>
+      })}
+    </ul>
+  )
+}
 
 export default function BundlesSidebarContent(
   {
@@ -10,27 +33,17 @@ export default function BundlesSidebarContent(
     elements: Array<Bundle>,
     baseHref: string
   }) {
-    return <>
-    {
-      elements.map((element: {name: string, bundles: Array<{path: string, name: string}>}) => {
-        return <div key={element.name}>
-          <h4>{element.name}</h4>
-          <ul>
-          {
-            element.bundles.map((subElement) => {
-              const slug = slugify(subElement.name)
-              return <li key={slug}>
-                <a href={baseHref + slug}>
-                  {subElement.name}
-                </a>
-              </li>
-            })
-          }
-          </ul>
-        </div>;
-      })
-    }
-    </>;
 
-
-}
+    return <div style={{marginTop: '2em'}}>
+      <h3 style={{ fontSize: '1.125rem'}}>Select bundle</h3>
+      <AccordionGroup>
+        {elements?.map((element: Bundle) => (
+          <Accordion
+            key={element.name}
+            title={ element.name }
+            content={renderAccordionContent(element.bundles, baseHref)}
+          />
+        ))}
+      </AccordionGroup>;
+    </div>
+  }
