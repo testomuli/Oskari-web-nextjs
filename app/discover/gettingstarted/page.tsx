0e737c7ab81b44067b82f71e5afe3c0cc8c3bb08
Pekka Helesuo <pekka.helesuo@siili.com>
2024-05-08 16:17:25 +0300

    add navigation and placeholder for getting stareted -section

---
+++ app/discover/gettingstarted/page.tsx
@@ -0,0 +1,7 @@
+import Layout from "@/components/Layout";
+
+export default function GettingStartedPage() {
+  return <Layout heroSmall heroTitle='Getting started'>
+    <div>Placeholder content for getting started.</div>
+  </Layout>;
+}
