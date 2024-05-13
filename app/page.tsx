c29db76a164896304a91156a94fd45034537c052
Pekka Helesuo <pekka.helesuo@siili.com>
2024-05-13 12:12:19 +0300

    remove useless sections from front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
 import allPosts from '@/_content/blog'
 import CardList from '@/components/Cards/CardList'
-import FeaturesList from '@/components/Features/FeaturesList'
 import Layout from '@/components/Layout'
-import UsedBy from '@/components/UsedBy'
-import featureItems from '@/utils/data/features.json'
-import usedbyItems from '@/utils/data/usedby.json'
 
 export default function HomePage() {
   const whatsnew = allPosts
@@ -18,8 +14,6 @@ export default function HomePage() {
     .slice(0, 4)
   return (
     <Layout>
-      <FeaturesList title='With Oskari you can...' data={featureItems} />
-      <UsedBy title='Used by' data={usedbyItems} />
       <CardList data={whatsnew} title='What’s new' />
     </Layout>
   )
