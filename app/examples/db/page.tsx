f9a0a75f2178d310bea65b9c96b678d82585d2f4
Pekka Helesuo <pekka.helesuo@siili.com>
2024-06-13 13:42:37 +0300

    add db docs placeholder page

---
+++ app/examples/db/page.tsx
@@ -0,0 +1,6 @@
+export default function DatabaseDocs() {
+  return <>
+    <h1>Database documentation dev placeholder</h1>
+    <div>This would be redirected to /db/ if we were in production.</div>
+  </>
+};
