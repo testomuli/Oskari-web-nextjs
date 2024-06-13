967a149de23acd62124b4c25230c169fe10b214e
Pekka Helesuo <pekka.helesuo@siili.com>
2024-06-13 16:52:06 +0300

    remove useless placeholder

--- app/examples/db/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function DatabaseDocs() {
-  return <>
-    <h1>Database documentation dev placeholder</h1>
-    <div>This would be redirected to /db/ if we were in production.</div>
-  </>
-};
