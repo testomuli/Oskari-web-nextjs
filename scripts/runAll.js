5dc5561786973f700d701a16733289ad6422a5ae
Pekka Helesuo <pekka.helesuo@siili.com>
2024-06-17 11:38:55 +0300

    do not automatically run 'latest' - docs

--- scripts/runAll.js
+++ scripts/runAll.js
@@ -3,16 +3,13 @@ const UNRELEASED_VERSION = 'unreleased';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { execSync } = require('child_process');
 
+// if not provided build "nightly build"
 const requestedVersion = process.argv.slice(2)[0] || UNRELEASED_VERSION
 
-// always regenerate 'latest'
-execSync('node scripts/generateDocs.js latest');
 execSync('node scripts/generateDocs.js ' + requestedVersion);
 execSync('node scripts/generateContentMetadata.js');
 execSync('node scripts/generateDocumentationMetadata.js');
 
-// always regenerate 'latest'
-execSync('node scripts/generateApidocs.js latest');
 execSync('node scripts/generateApidocs.js ' + requestedVersion);
 
 //run db generation script
