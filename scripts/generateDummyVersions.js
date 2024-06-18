39b6760b5f90a4a6ed9ccd44016281ce1178d37f
Sami Mäkinen <sami.makinen@nls.fi>
2024-06-18 18:06:18 +0300

    Generate dummy version doc index

--- scripts/generateDummyVersions.js
+++ scripts/generateDummyVersions.js
@@ -3,12 +3,21 @@ const path = require('path');
 const { generateDocumentationMetadata } = require('./documentationMetadataHelper');
 
 // So build on GitHub Actions can work without cloning the documentation folders
-const generateDummyDocs = (folder) => {
+const generateDummyDocs = (folder, addIndex = true) => {
     if (!fs.existsSync(folder)) {
         fs.mkdirSync(folder, { recursive: true });
     }
-    generateDocumentationMetadata(folder);
+    
+    addIndex && generateDocumentationMetadata(folder);
+    
 };
 
 generateDummyDocs('./_content/api/versions/');
+generateDummyDocs('./_content/api/versions/latest', false);
+const indexContent = `
+const allDocs = [];
+export default allDocs;
+`;
+fs.writeFileSync(path.join('./_content/api/versions/latest/', 'index.js'), indexContent);
+
 generateDummyDocs('./_content/docs/');
