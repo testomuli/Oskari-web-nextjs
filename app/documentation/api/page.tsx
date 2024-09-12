ef5e0986bf864916b240aae9237dbe8583041012
Pekka Helesuo <pekka.helesuo@siili.com>
2024-09-12 14:36:21 +0300

    highlightbox item alignment & bgcolor same as in faq

--- app/documentation/api/page.tsx
+++ app/documentation/api/page.tsx
@@ -6,15 +6,14 @@ import Text from '@/components/Text'
 export default function ApiMainPage() {
   return <>
     <DefaultLayout heroSmall heroTitle='Oskari API'>
-      <div>
         <Text>
           Oskari API is defined as bundles. Bundle is a definition of functionality that may provide external API to control the functionality.
         </Text>
-        <HighlightBox>
-          <div style={{ display: 'flex', flexDirection: 'column'}}>
+        <HighlightBox style= {{ backgroundColor: '#FAFAFA'}}>
+          <div style={{ display: 'flex', flexDirection: 'column', maxWidth: '85vw'}}>
             <h2 style={{fontSize: '1.625rem'}}>Building a custom Oskari application</h2>
             <div>
-              <p>Choose the functionalities from bundle documentation</p>
+              <p className="mt-2 mb-2">Choose the functionalities from bundle documentation</p>
               <Button
                 variant='primary'
                 title='Bundle documentation'
@@ -22,29 +21,25 @@ export default function ApiMainPage() {
             </div>
             <div style={{ marginTop: '2em'}}>
               <h2 style={{fontSize: '1.625rem'}}>Working with an existing Oskari application</h2>
-              <div style={{ gridColumn: '2' }} className='flex justify-center'>
-                <div className='flex w-full justify-center gap-4 items-center'>
-                  <div>
-                    <p>Controlling existing functionality</p>
-                    <Button
-                      variant='primary'
-                      title='Requests documentation'
-                      href={'/documentation/api/requests'}/>
-                  </div>
-                  <div>
-                    <p>Reacting to events</p>
-                    <Button
-                      variant='primary'
-                      title='Events documentation'
-                      href={'/documentation/api/events'}/>
-                  </div>
+              <div className='flex flex-col'>
+                <div className="mt-3 mb-3">
+                  <p className="mt-2 mb-2">Controlling existing functionality</p>
+                  <Button
+                    variant='primary'
+                    title='Requests documentation'
+                    href={'/documentation/api/requests'}/>
                 </div>
-
+                <div>
+                  <p className="mt-2 mb-2">Reacting to events</p>
+                  <Button
+                    variant='primary'
+                    title='Events documentation'
+                    href={'/documentation/api/events'}/>
+                </div>
               </div>
             </div>
           </div>
         </HighlightBox>
-      </div>
     </DefaultLayout>
   </>;
 }
