6d93f8e82adfd79a3ca1b48548fa4e60f3391ac1
rekjuh <87303508+rekjuh@users.noreply.github.com>
2024-11-25 17:25:26 +0200

    Update
    Contact coordinators beforehand ...

--- app/discover/procurement/page.tsx
+++ app/discover/procurement/page.tsx
@@ -285,7 +285,7 @@ If you want to create new functionalities for Oskari, it might be a good idea to
 </Text>
 
 <Text>
-With new functionalities, one should remember that they must be maintained. When you create new functionalities to your Oskari service, remember to inform NLS both beforehand and after the development has taken place. <Link href='/contribute'>By contacting Oskari project coordinators</Link> before you start the development, your developers can get best practices for the development process and further ideas on how to create the functionalities that you want. 
+With new functionalities, one should remember that they must be maintained. When you create new functionalities to your Oskari service, it could be beneficial <Link href='/contribute'>to contact the project coordinators</Link> beforehand. They might be able to offer feedback and help coordinate development of similar functionalities if you contact them before you start the development.Your developers can get best practices for the development process of Oskari and further ideas on how to create the functionalities. 
 </Text>
 
 <Text>
